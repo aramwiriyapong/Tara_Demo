730cb0db8da7c8a95d8c8a5f3fcb63eb92399b7d
pitula aramwiriyapong <pitula.a@outlook.com>
2021-09-29 03:43:18 +0700

    Add project files.

---
+++ Tara_Demo/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Tara_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("ABBvoice", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(32, 232);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("ABBvoice", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cycle Time";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("ABBvoice", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 88);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(273, 129);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Robot Data";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(158, 74);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(92, 28);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "0";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(158, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(92, 28);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("ABBvoice", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(17, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Counter";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Tara_Demo.Properties.Resources._2000px_ABB_logo_svg;
+            this.pictureBox1.Location = new System.Drawing.Point(210, 243);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(75, 29);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel1.Location = new System.Drawing.Point(12, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(32, 5);
+            this.panel1.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 49);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(192, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button3.Font = new System.Drawing.Font("ABBvoice", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(210, 49);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 21);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Connect";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(297, 295);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Tara Demo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Tara_Demo/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ABB.Robotics.Controllers;
+using ABB.Robotics.Controllers.Discovery;
+using ABB.Robotics.Controllers.RapidDomain;
+using ABB.Robotics.Controllers.IOSystemDomain;
+
+namespace Tara_Demo
+{
+    public partial class Form1 : Form
+    {
+        private NetworkScanner scanner = null;
+        private Controller controller = null;
+        private NetworkWatcher networkwatcher = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.scanner = new NetworkScanner();
+            scanner.Scan();
+            ControllerInfoCollection controllers = scanner.Controllers;
+            this.networkwatcher = new NetworkWatcher(scanner.Controllers);
+            this.networkwatcher.Found += new EventHandler<NetworkWatcherEventArgs>(HandleFoundEvent);
+
+            this.networkwatcher.EnableRaisingEvents = true;
+
+
+
+            foreach (ControllerInfo controllerInfo in controllers)
+            {
+
+                comboBox1.Items.Add(controllerInfo.SystemName);
+                comboBox1.Tag = controllerInfo;
+
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+
+
+        }
+        void HandleFoundEvent(object sender, NetworkWatcherEventArgs e)
+        {
+            this.Invoke(new
+            EventHandler<NetworkWatcherEventArgs>(AddControllerToListView),
+            new Object[] { this, e });
+        }
+        private void AddControllerToListView(object sender, NetworkWatcherEventArgs e)
+        {
+            ControllerInfo controllerInfo = e.Controller;
+            comboBox1.Items.Add(controllerInfo.ControllerName);
+            comboBox1.Tag = controllerInfo;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.controller != null)
+            {
+                Signal signal1 = controller.IOSystem.GetSignal("diStart");
+                DigitalSignal diSig = (DigitalSignal)signal1;
+                if (button1.Text == "Start")
+                {
+                    diSig.Set();
+                    button1.Text = "Stop";
+                }
+                else if (button1.Text == "Stop")
+                {
+                    diSig.Reset();
+                    button1.Text = "Start";
+                }
+            }
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+
+            if (comboBox1.SelectedText != null)
+            {
+                ControllerInfo controllerInfo = (ControllerInfo)comboBox1.Tag;
+                if (controllerInfo.Availability == Availability.Available)
+                {
+                    if (this.controller != null)
+                    {
+                        this.controller.Logoff();
+                        this.controller.Dispose();
+                        this.controller = null;
+                        button3.Text = "Connect";
+                    }
+                    else
+                    {
+                        this.controller = ControllerFactory.CreateFrom(controllerInfo);
+                        this.controller.Logon(UserInfo.DefaultUser);
+
+                        CheckSignal();
+                        button3.Text = "Disconnect";
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Selected controller not available.");
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void CheckSignal()
+        {
+            Signal signal1 = controller.IOSystem.GetSignal("diStart");
+            DigitalSignal diSig = (DigitalSignal)signal1;
+            if (diSig.Get() == 1)
+            {
+                button1.Text = "Stop";
+            }
+            else
+            {
+                button1.Text = "Start";
+            }
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (this.controller != null)
+            {
+                RapidData rd1 = controller.Rapid.GetRapidData("T_ROB1", "Module1", "nCycleTime");
+                if (rd1.Value is ABB.Robotics.Controllers.RapidDomain.Num)
+                {
+                    textBox1.Text = rd1.Value.ToString();
+                }
+
+                RapidData rd2 = controller.Rapid.GetRapidData("T_ROB1", "Module1", "nCounter");
+                if (rd2.Value is ABB.Robotics.Controllers.RapidDomain.Num)
+                {
+                    textBox2.Text = rd2.Value.ToString();
+                }
+            }
+        }
+
+    }
+}
